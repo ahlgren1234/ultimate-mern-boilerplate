04fbee161cac3ddde72aa4c1120b8ae1b7aabf20
Peter Ahlgren <ahlgren1234@gmail.com>
2021-09-30 11:20:24 +0200

    setting up middleware and server env

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/api/signup", (req, res) => {
-  res.json({
-    data: "you hit signup endpoint",
-  });
-});
+// import routes
+const authRoutes = require("./routes/auth");
 
-const port = process.env.port || 8000;
+// app middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+//app.use(cors()); // allow all origins
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
+// middleware
+app.use("/api", authRoutes);
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+  console.log(`API is running on port ${port} - ${process.env.NODE_ENV}`);
 });
