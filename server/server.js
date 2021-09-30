4c0a53a681ec1b0157a992622b4f824f132e8cfc
Peter Ahlgren <ahlgren1234@gmail.com>
2021-09-30 12:37:41 +0200

    Add validation on signup

--- server/server.js
+++ server/server.js
@@ -7,6 +7,17 @@ require("dotenv").config();
 
 const app = express();
 
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
 // import routes
 const authRoutes = require("./routes/auth");
 
